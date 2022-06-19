4da454321355eda7d41aaa1811aa8e8053a9d5f5
Sungro9703 <102012075+Sungro9703@users.noreply.github.com>
2022-06-19 15:32:49 +0900

    Add files via upload

---
+++ progrmmers - JAVA/programmers01/javaStudy02/Car.java
@@ -0,0 +1,8 @@
+package javaStudy02;
+
+public class Car {
+
+	// Car의 속성 = 필드
+	String name;
+	int number;
+}
---
+++ progrmmers - JAVA/programmers01/javaStudy02/ReferenceDateType.java
@@ -0,0 +1,11 @@
+package javaStudy02;
+
+public class ReferenceDateType {
+
+	public static void main(String[] args) {
+		int i = 4;
+		
+		String str = new String("hello");	// new 생성자
+		
+	}
+}
