70942e631ae2835bf2810c34ce40445429614791
Sungro9703 <102012075+Sungro9703@users.noreply.github.com>
2022-06-20 23:45:46 +0900

    Add files via upload

---
+++ progrmmers - JAVA/programmers01/javaStudy02/CarExam2.java
@@ -0,0 +1,14 @@
+package javaStudy02;
+
+public class CarExam2 {
+	
+	public static void main(String[] args) {
+		Car car1 = new Car("소방차");
+		Car car2 = new Car("구급차");
+		
+		System.out.println(car1.name);
+	}
+	
+
+	
+}
